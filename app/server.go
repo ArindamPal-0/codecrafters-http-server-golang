db506d8aab5df3c368dd7eb2093a0519c5ae40fa
Arindam Pal <arindampal2000@gmail.com>
2023-10-01 20:22:27 +0530

    feat: respond with content
    fix: code refactoring of struct Request, Response

--- app/server.go
+++ app/server.go
@@ -7,6 +7,123 @@ import (
 	"strings"
 )
 
+type Request struct {
+	Method      string
+	Path        string
+	HttpVersion string
+	Headers     map[string]string
+	Body        []byte
+}
+
+func ParseRequest(requestStr string) Request {
+
+	lines := strings.Split(requestStr, "\r\n")
+
+	// parse the request start line
+	// http request status line
+	startLineSplit := strings.Split(lines[0], " ")
+
+	// http method
+	httpMethod := startLineSplit[0]
+
+	// http request path
+	httpPath := startLineSplit[1]
+
+	// http version
+	httpVersion := startLineSplit[2]
+
+	// http headers
+	headers := map[string]string{}
+
+	// http body
+	body := []byte{}
+
+	return Request{
+		Method:      httpMethod,
+		Path:        httpPath,
+		HttpVersion: httpVersion,
+		Headers:     headers,
+		Body:        body,
+	}
+}
+
+type StatusCode string
+
+const (
+	Status_NotFound StatusCode = "404 Not Found"
+
+	Status_OK = "200 OK"
+)
+
+type Response struct {
+	HttpVersion string
+	StatusCode  StatusCode
+	Headers     map[string]string
+	Body        []byte
+}
+
+func (response *Response) getHeadersBytes() []byte {
+	headerBytes := []byte{}
+	for k, v := range response.Headers {
+		headerStr := fmt.Sprintf("%s: %s\r\n", k, v)
+		headerBytes = append(headerBytes, []byte(headerStr)...)
+	}
+
+	return headerBytes
+}
+
+func (response *Response) getResponseBytes() []byte {
+	// append http response status line
+	statusLine := fmt.Sprintf("%s %s\r\n", response.HttpVersion, response.StatusCode)
+	responseBytes := []byte(statusLine)
+
+	// append http response headers
+	headersBytes := response.getHeadersBytes()
+	responseBytes = append(responseBytes, headersBytes...)
+
+	// append a new line
+	responseBytes = append(responseBytes, []byte("\r\n")...)
+	// append body
+	responseBytes = append(responseBytes, response.Body...)
+
+	return responseBytes
+}
+
+func (response *Response) Send(conn net.Conn) {
+	responseBytes := response.getResponseBytes()
+	_, err := conn.Write(responseBytes)
+	if err != nil {
+		fmt.Println("Error sending response: ", err.Error())
+		os.Exit(1)
+	}
+}
+
+func Router(request *Request) Response {
+	// default response
+	response := Response{
+		HttpVersion: request.HttpVersion,
+		StatusCode:  Status_NotFound,
+		Headers:     map[string]string{},
+		Body:        []byte{},
+	}
+
+	if request.Path == "/" {
+		response.StatusCode = Status_OK
+	} else if strings.HasPrefix(request.Path, "/echo/") {
+		echoText := strings.TrimPrefix(request.Path, "/echo/")
+
+		fmt.Println("> echo text: ", echoText)
+		response.StatusCode = Status_OK
+		response.Headers = map[string]string{
+			"Content-Type":   "text/plain",
+			"Content-Length": fmt.Sprintf("%d", len(echoText)),
+		}
+		response.Body = []byte(echoText)
+	}
+
+	return response
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -31,6 +148,8 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	fmt.Println("> Request")
+
+	// read request
 	requestBytes := make([]byte, 1024)
 	requestLength, err := conn.Read(requestBytes)
 	if err != nil {
@@ -42,25 +161,12 @@ func handleConnection(conn net.Conn) {
 	requestStr := string(requestBytes[:requestLength])
 	fmt.Println(requestStr)
 
-	lines := strings.Split(requestStr, "\r\n")
+	// parse request
+	request := ParseRequest(requestStr)
 
-	// http request status line
-	statusLineSplit := strings.Split(lines[0], " ")
+	// get response
+	response := Router(&request)
 
-	// http request path
-	httpPath := statusLineSplit[1]
-
-	var responseBytes []byte
-	if httpPath == "/" {
-		responseBytes = []byte("HTTP/1.1 200 OK\r\n\r\n")
-	} else {
-		responseBytes = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
-	}
-
-	_, err = conn.Write(responseBytes)
-	if err != nil {
-		fmt.Println("Error sending response: ", err.Error())
-		os.Exit(1)
-	}
-
+	// send response
+	response.Send(conn)
 }
