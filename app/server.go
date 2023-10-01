76cfb7ea414c7b6cf80bd18a40a6a83e8c23b5d8
Arindam Pal <arindampal2000@gmail.com>
2023-10-01 17:20:16 +0530

    feat: responding with 200 status

--- app/server.go
+++ app/server.go
@@ -12,10 +12,26 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	_, err = l.Accept()
+	fmt.Println("Listening on http://127.0.0.1:4221")
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+
+	requestBytes := []byte{}
+	_, err = conn.Read(requestBytes)
+	if err != nil {
+		fmt.Println("Error reading request: ", err.Error())
+	}
+
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error sending response: ", err.Error())
+	}
+
 }
